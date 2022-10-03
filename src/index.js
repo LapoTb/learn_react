a2a4b422446d87b9318541a0bf559b6bfca8e34a
LapoTb <LapoTb@mail.ru>
2022-10-03 16:27:20 +0800

    Init employees project. Step 0

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-
-const elem = <h2>Hello World</h2>;
+import App from './components/app/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  //elem
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
